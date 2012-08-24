3f04a488ed0678e19a9cbdff0e7c17dcc068fa74
Michael Henke <github@codingmerc.com>
2012-08-23 20:20:08 -0700

    Update for MAC OSX Mountain Lion 10.8
    git diff
    diff --git a/raspiwrite.py b/raspiwrite.py
    index 054f93a..f765719 100755
    --- a/raspiwrite.py
    +++ b/raspiwrite.py
    @@ -172,10 +172,10 @@ class transferInBackground (threading.Thread):    #Runs the dd command in a thread
        def run ( self ):
            global SDsnip
            global path
    -       if OS[0] != 'Darwin':
    -               copyString = 'dd bs=1M if=%s of=%s' % (path,SDsnip)
    -       else
    -               copyString = 'dd bs=1m if=%s of=%s' % (path,SDsnip)
    +       if OS[0] != 'Darwin' :
    +                       copyString = 'dd bs=1M if=%s of=%s' % (path,SDsnip)
    +       else: 
    +                       copyString = 'dd bs=1m if=%s of=%s' % (path,SDsnip)
            print 'Running ' + copyString + '...'
     
            print getoutput(copyString)
    @@ -272,10 +272,10 @@ def transfer(file,archiveType,obtain,SD,URL):     #unzips the disk image
                    SDsnip = "/dev/mmcblk" + SD[11]
            else:
                    if OS[0] != 'Darwin': 
    -               SDsnip =  SD.replace(' ', '')[:-1]
    +                       SDsnip =  SD.replace(' ', '')[:-1]
                    else:
                            # remove weird partition notation in OS X partition names
    -               SDsnip =  SD.replace(' ', '')[:-2]
    +                       SDsnip =  SD.replace(' ', '')[:-2]
     
            print path
            print '\n\n###################################################################'

--- raspiwrite.py
+++ raspiwrite.py
@@ -173,9 +173,9 @@ class transferInBackground (threading.Thread): 	#Runs the dd command in a thread
 	global SDsnip
 	global path
 	if OS[0] != 'Darwin':
-		copyString = 'dd bs=1M if=%s of=%s' % (path,SDsnip)
-	else
-		copyString = 'dd bs=1m if=%s of=%s' % (path,SDsnip)
+			copyString = 'dd bs=1M if=%s of=%s' % (path,SDsnip)
+	else:
+			copyString = 'dd bs=1m if=%s of=%s' % (path,SDsnip)
 	print 'Running ' + copyString + '...'
 
 	print getoutput(copyString)
@@ -272,10 +272,10 @@ def transfer(file,archiveType,obtain,SD,URL):	#unzips the disk image
 		SDsnip = "/dev/mmcblk" + SD[11]
 	else:
 		if OS[0] != 'Darwin': 
-        	SDsnip =  SD.replace(' ', '')[:-1]
+        		SDsnip =  SD.replace(' ', '')[:-1]
  		else:
  			# remove weird partition notation in OS X partition names
-        	SDsnip =  SD.replace(' ', '')[:-2]
+        		SDsnip =  SD.replace(' ', '')[:-2]
 
 	print path
 	print '\n\n###################################################################'
